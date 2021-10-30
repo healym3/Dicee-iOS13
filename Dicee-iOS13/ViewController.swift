326d05b5df4f5cc1875138e5fe4ae4b7f8681244
healym3 <79485702+healym3@users.noreply.github.com>
2021-10-30 13:52:23 -0500

    Add Random Dice Functionality

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    let images = [UIImage(named:"DiceOne"),UIImage(named:"DiceTwo"),UIImage(named:"DiceThree"),UIImage(named:"DiceFour"), UIImage(named:"DiceFive"), UIImage(named:"DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        //print("Button got tapped.")
+        
+        diceImageView1.image = images[Int.random(in:0...5)]
+        //diceImageView1.alpha = 0.5
+        diceImageView2.image = images[Int.random(in:0...5)]
+        
+    }
+        
 }
 
